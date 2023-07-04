63eb544685e0000df67daffe76f385e94973cff6
Voltrein-code <mr-votlrein@yandex.ru>
2023-07-04 10:52:14 +0300

    fix: Фикс синтаксиса jsx

--- src/components/LeadSection/LeadSection.js
+++ src/components/LeadSection/LeadSection.js
@@ -3,17 +3,17 @@ import Smartphone from "../Smartphone/Smartphone";
 
 export default function LeadSection() {
   return (
-    <section classNameName="lead">
-      <div classNameName="lead__container">
-        <div classNameName="lead__heading">
-          <h1 classNameName="lead__slogan">
+    <section className="lead">
+      <div className="lead__container">
+        <div className="lead__heading">
+          <h1 className="lead__slogan">
             Разработка, внедрение, консультация, поддержка
           </h1>
-          <p classNameName="lead__text">
+          <p className="lead__text">
             Наша компания оказывает ИТ услуги в разных областях. Все от
             проектирования и разработки до внедрения и сопровождения
           </p>
-          <ul classNameName="lead__buttons">
+          <ul className="lead__buttons">
             <li>
               <Button styles="button button_type_order" withRocket={true}>
                 Написать нам
@@ -24,8 +24,8 @@ export default function LeadSection() {
             </li>
           </ul>
         </div>
-        <div classNameName="lead__visuals">
-          <div classNameName="lead__star lead__star_color_white"></div>
+        <div className="lead__visuals">
+          <div className="lead__star lead__star_color_white"></div>
           <div className="lead__star lead__star_color_red"></div>
           <Smartphone />
         </div>
