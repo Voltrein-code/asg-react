d80505885ab5679305a968479bc11de03ee5bc80
Voltrein-code <mr-votlrein@yandex.ru>
2023-07-04 13:43:12 +0300

    feat: Добавить компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import Header from "../Header/Header";
 import "./App.css";
 import Preloader from "../Preloader/Preloader";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -26,6 +26,7 @@ function App() {
         <div className={visible ? "app app_visible" : "app"}>
           <Header />
           <Main />
+          <Footer />
         </div>
       )}
     </>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <div class="footer__container">
+        <p class="footer__text">@ 2023 АСГ48</p>
+      </div>
+    </footer>
+  );
+}
