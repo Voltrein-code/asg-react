d2c50bb7e686da13151c434c580ba3dac7625ceb
Voltrein-code <mr-votlrein@yandex.ru>
2023-08-08 08:27:50 +0300

    feat: Добавить стили для оверлея

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import "./Modal.css";
 import ModalOverlay from "../ModalOverlay/ModalOverlay";
 
 const modalRoot = document.getElementById("modals");
--- src/components/ModalOverlay/ModalOverlay.js
+++ src/components/ModalOverlay/ModalOverlay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ModalOverlay.css";
 
 const ModalOverlay = ({ onClick }) => {
   return <div className="overlay" onMouseDown={onClick} />;
