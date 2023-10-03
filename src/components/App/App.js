95efa2ca20f2a37aae4ae6228e5400544996d66c
Voltrein-code <mr-votlrein@yandex.ru>
2023-10-03 10:28:23 +0300

    feat: Добавить описание и иконки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/order */
 import { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import "./App.css";
 import Preloader from "../Preloader/Preloader";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Helmet } from "react-helmet";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -20,6 +23,18 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Наша компания оказывает ИТ услуги в разных областях. Все от проектирования и разработки до внедрения и сопровождения"
+        />
+        <link rel="icon" href="../../media/logo.ico" type="any" />
+        <link rel="icon" href="../../media/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="../../media/logo.png" />
+        <title>АГС48</title>
+      </Helmet>
       {loading ? (
         <Preloader />
       ) : (
